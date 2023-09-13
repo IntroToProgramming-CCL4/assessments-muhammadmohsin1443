6049031563ae365b57813c4a2fe3c21c4d259d5b
muhammadmohsin1443 <144930856+muhammadmohsin1443@users.noreply.github.com>
2023-09-13 19:13:38 +0400

    Add files via upload

---
+++ Introduction/introduction.py
@@ -0,0 +1,4 @@
+print ('hi my name is muhammad moshin')
+print ('i am 18 years old')
+print ('currently a student of creative computing')
+print ('my hobbies are 3d rendering, football, gym, cooking, and drawing')
